ec0fc1b78451cea0547e78e0a674b75897452ab7
Mateus Fernandes <mateus.fernandes06@gmail.com>
2018-01-21 23:54:56 -0500

    fix to docker push

--- docker.push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker build . -t s3analyser:${TRAVIS_COMMIT}
-docker tag s3analyser s3analyser/s3analyser:${TRAVIS_TAG};
-docker login -u "$DOCKER_SAT_USERNAME" -p "$DOCKER_SAT_PASSWORD";
-docker push s3analyser/s3analyser:${TRAVIS_TAG};
